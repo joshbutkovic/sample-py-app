52b1e87f7dda1614360ae114030e00a6a0d9c550
joshbutkovic <joshbutkovic@gmail.com>
2019-11-18 21:38:16 -0800

    EOD monday commit imported BeautifulSoup request

--- pycrunch/cli/cli.py
+++ pycrunch/cli/cli.py
@@ -1,3 +1,9 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+
 class Cli:
 
     def __init__(self):
@@ -14,4 +20,15 @@ class Cli:
 
     def get_choice(self):
         choice = input()
-        print(f"Your choice was: {choice}")
+        print(f"Your choice was: {choice}\nProcessing...")
+        self.process_choice()
+
+    def process_choice(self):
+        url = 'http://www.nfl.com/stats/categorystats?tabSeq=2&defensiveStatisticCategory=GAME_STATS&conference=ALL&role=OPP&season=2019&seasonType=REG&d-447263-s=TOTAL_YARDS_GAME_AVG&d-447263-o=1&d-447263-n=1'
+        response = requests.get(url)
+        # print(response.text)
+        soup = BeautifulSoup(response.text, "html.parser")
+        soup_table = soup.find_all('table')
+        print(soup_table)
+        # time.sleep(1)
+        # print(soup.find_all('a'))
